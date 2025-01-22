0a041c1a59310050122fee285a220462e33ee1a0
Chanuya <97076454+chanwoo1@users.noreply.github.com>
2025-01-22 19:35:59 +0900

    [Bronze V] Title: A+B, Time: 176 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int intValue1 = scanner.nextInt();
+        int intValue2 = scanner.nextInt();
+        System.out.println(intValue1 + intValue2);
+
+    }
+}
