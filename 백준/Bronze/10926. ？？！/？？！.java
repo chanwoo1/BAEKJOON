7e90d067f442cc1527438a8c3b649329f71a4397
Chanuya <97076454+chanwoo1@users.noreply.github.com>
2025-01-22 19:50:16 +0900

    [Bronze V] Title: ??!, Time: 184 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.nextLine();
+
+        System.out.println(str + "??!");
+
+
+
+
+    }
+}
