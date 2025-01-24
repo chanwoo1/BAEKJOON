b1dafeca5fc88bf795edf8d30b6ad4be86ff3897
Chanuya <97076454+chanwoo1@users.noreply.github.com>
2025-01-24 16:39:54 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 240 ms, Memory: 18092 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int star = scanner.nextInt();
+
+        for (int i = 1; i <= star; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
